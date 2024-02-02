65a1625a978d64d7861ebc43cb6bee0052224a0b
adit <aditsharma1203@gmail.com>
2024-02-03 01:55:32 +0530

    added create note button on the frontpage

--- routes/static.js
+++ routes/static.js
@@ -9,7 +9,24 @@ const router = express.Router();
 
 
 router.get("/", controlGetAll)
+router.get("/new/", (req,res)=>{
+    res.render("create");
+})
 router.get("/:id", controlGetSpecific)
+
+
+router.post("/", async (req,res)=>{
+    const {title,description} = req.body;
+
+    await axios.post("http://localhost:8000/api/notes", {
+        title: title,
+        description: description
+    })
+    .then(response=>console.log(response))
+    .catch(err=>console.log(err));
+
+    res.redirect("http://localhost:8000/");
+})
 
 router.patch("/:id", controlUpdateSpecific )
 
